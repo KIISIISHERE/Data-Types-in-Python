2ba4a503beb87529bcf15c745eeb6c5cb6f307d3
KIISIISHERE <iteoluwakiisi420@gmail.com>
2025-03-11 18:53:28 +0000

    I made a code

---
+++ main.py
@@ -0,0 +1,15 @@
+#create different variables and find out their data types
+name="Iteoluwa"
+age="11+"
+grade="7"
+height=4.8
+is_student=True
+print("The datatype of name is,",type(name))
+print("The datatype of age is,",type(age))
+print("The datatype of grade is,",type(grade))
+print("The datatype of height is,",type(height))
+print("The datatype of is_student is,",type(is_student))
+#Type casting
+print("************************************************")
+print("The new datatype for name is,",type(int(name))) #convert the name into an integer
+print("The new datatype for age is,",type(str(age))) #convert the age into a string
---
+++ main2.py
@@ -0,0 +1,3 @@
+#ask the user for their name, print a welcome message
+name=input("Enter your name: ")
+print("Hello",name," welcome to my program")
